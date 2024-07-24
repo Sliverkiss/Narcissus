f9f378889dabfcf16c89dab67572baa8ef22cbb8
Sliverkiss <70052878+Sliverkiss@users.noreply.github.com>
2024-07-24 18:56:49 +0800

    add helloPlugin

---
+++ src/plugins/helloPlugin.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'Hello Plugin',
+    execute: (ctx) => {
+      if (ctx.message && ctx.message.text === '/start') {
+        logger.info("测试全局调用成功！")
+        ctx.reply('Hello World!');
+      }
+    }
+  };
