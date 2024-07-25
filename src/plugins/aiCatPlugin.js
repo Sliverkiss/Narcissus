403591e8046d6c8e4969c8808ad3e9b1c228566f
Sliverkiss <70052878+Sliverkiss@users.noreply.github.com>
2024-07-25 10:20:58 +0800

    add：增加猫娘ai插件

--- src/plugins/aiCatPlugin.js
+++ src/plugins/aiCatPlugin.js
@@ -2,8 +2,8 @@ module.exports = {
     name: '【猫娘AI】',
     promise:'personal',
     execute: async (ctx) => {
-        if (ctx.message && ctx.message.text.startsWith(',cat')) {
-            const text = ctx?.message?.reply_to_message?.text || ctx.message.text.split(' ').slice(1).join(' ');
+        if (ctx.message && ctx?.message?.text?.startsWith(',cat')) {
+            const text =ctx?.message?.text?.split(' ').slice(1).join(' ')||ctx?.message?.reply_to_message?.text;
             let message = await ctx.reply("🐱正在思考中 . . .");
             try {
                 const response = await fetch(`https://api.mhimg.cn/api/gpt_aimaoniang/?prompt=${encodeURIComponent(text)}`);
