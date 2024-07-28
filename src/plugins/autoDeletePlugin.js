aaea1a34e99952229e7ccb0c1221a14b1623bab7
Sliverkiss <70052878+Sliverkiss@users.noreply.github.com>
2024-07-28 22:44:23 +0800

    feat(plugins):优化bot回复及相关日志
    1. 当检测到指令时，bot会自动在指令下方进行回复。如指令是对其他消息的回复，则bot会在该消息下方进行回复。
    2. 当检测到指令、回复消息、删除消息时会输出相关日志信息

--- src/plugins/autoDeletePlugin.js
+++ src/plugins/autoDeletePlugin.js
@@ -6,23 +6,30 @@ module.exports = {
         const originalReply = ctx.reply.bind(ctx);
         // 重写 ctx.reply 方法
         ctx.reply = async (...args) => {
-            const sentMessage = await originalReply(...args);
+            const options = args[1] || {};
+            // 如果 ctx.message 存在，添加 reply_to_message_id 选项
+            if (ctx.message && ctx.message.message_id) {
+                options.reply_to_message_id = ctx?.message?.reply_to_message?.message_id || ctx.message.message_id;
+            }
+            // 调用原始的 ctx.reply 方法并传递修改后的选项
+            const sentMessage = await originalReply(args[0], options);
             // 检查是否是 bot 发送的消息
             if (sentMessage && sentMessage?.message_id) {
                 const messageId = sentMessage?.message_id;
                 const chatId = sentMessage?.chat.id;
-
-                console.log(`Bot's message ID: ${messageId} in chat ${chatId}`);
+                let command=ctx?.message?.text?.split(" ")[0];
+                logger.info(`检测到指令:${command}`);
+                logger.info(`记录消息 -> Bot's message ID: ${messageId} in chat ${chatId}`);
 
                 // 设置定时删除
                 setTimeout(async () => {
                     try {
                         await ctx.telegram.deleteMessage(chatId, messageId);
-                        console.log(
-                            `Bot's message ID: ${messageId} deleted from chat ${chatId}`
+                        logger.info(
+                            `删除消息 -> Bot's message ID: ${messageId} deleted from chat ${chatId}`
                         );
                     } catch (error) {
-                        console.error(
+                        logger.error(
                             `Failed to delete bot's message ID: ${messageId} from chat ${chatId}`,
                             error
                         );
