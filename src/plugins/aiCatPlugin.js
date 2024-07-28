3eb6a5bf706904430784d96ec6a8b349dadbbba0
Sliverkiss <70052878+Sliverkiss@users.noreply.github.com>
2024-07-28 23:10:05 +0800

    feat(plugins): 增加ai自动回复功能
    1. 当别人在bot消息下方回复时，bot会自动调用ai思考并回复消息
    2. 当调用,cat指令时，可以主动触发ai思考并回复消息

--- src/plugins/aiCatPlugin.js
+++ src/plugins/aiCatPlugin.js
@@ -1,16 +1,30 @@
 module.exports = {
     name: '【猫娘AI】',
-    promise: 'personal',
+    promise: "personal",
     execute: async (ctx) => {
         try {
-            if (ctx.message && ctx?.message?.text?.startsWith(',cat')) {
+            //自动回复功能
+            if (isAutoReply(ctx)) {
+                const text = ctx?.message?.text
+                try {
+                    const response = await fetch(`https://api.mhimg.cn/api/gpt_aimaoniang/?prompt=${encodeURIComponent(text)}`);
+                    const responseText = await response.text();
+                    logger.info(`ai自动回复：${responseText}`);
+                    await ctx.reply(responseText);
+                } catch (error) {
+                    logger.error(error.toString());
+                }
+                return;
+            }
+            //指令问答功能
+            if (isCommand(ctx)) {
                 const text = ctx?.message?.reply_to_message?.text || ctx.message.text.split(' ').slice(1).join(' ');
                 let message = await ctx.reply("🐱正在思考中 . . .");
                 logger.info(text);
                 try {
                     const response = await fetch(`https://api.mhimg.cn/api/gpt_aimaoniang/?prompt=${encodeURIComponent(text)}`);
                     const responseText = await response.text();
-                    logger.info(responseText);
+                    logger.info(`ai自动回复：${responseText}`);
                     await ctx.telegram.editMessageText(
                         message.chat.id,
                         message.message_id,
@@ -29,5 +43,14 @@ module.exports = {
         } catch (e) {
             logger.error(e);
         }
+
     }
 };
+
+function isAutoReply(ctx) {
+    return ctx?.message && ctx?.chat?.type != 'private' && ctx?.message?.reply_to_message?.from?.id == '7097873325'
+}
+
+function isCommand(ctx) {
+    return ctx?.message && ctx?.message?.text?.startsWith(',cat');
+}
