0e1203004f61e5dc602319c3269c51279110f24a
大老赖不赖 <laiorigin@icloud.com>
2024-08-04 18:07:59 +0800

    feat(logger): warn

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,8 +2,20 @@ const logger = {
   info: (message) => {
     console.log(`[INFO] ${new Date().toISOString()} - ${message}`);
   },
+  warn: (message, error) => {
+    //todo 未来提高复用性
+    if (error) {
+      console.warn(`[WARN] ${new Date().toISOString()} - ${message}`, error);
+    }else {
+      console.warn(`[WARN] ${new Date().toISOString()} - ${message}`);
+    }
+  },
   error: (message, error) => {
-    console.error(`[ERROR] ${new Date().toISOString()} - ${message}`, error);
+    if (error) {
+      console.error(`[ERROR] ${new Date().toISOString()} - ${message}`, error);
+    }else{
+      console.error(`[ERROR] ${new Date().toISOString()} - ${message}`);
+    }
   }
 };
 
