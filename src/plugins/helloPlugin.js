840194038cb26ab20880875d33e9d41f85a4322d
Sliverkiss <70052878+Sliverkiss@users.noreply.github.com>
2024-08-26 16:10:21 +0800

    feat(plugin):add button of helloPlugin

--- src/plugins/helloPlugin.js
+++ src/plugins/helloPlugin.js
@@ -6,9 +6,18 @@ export default {
             if ($.command(ctx, '/start')) {
                 const userId = ctx?.message?.from?.id;
                 const userName = ctx?.message?.from?.first_name;
-                const caption = `☆　　╱╲*╱╲　☆\n ╱╳+▲╱　　╲　☆\n╱╱ ◢◣+　　╳╲ \n╱ +◢█◣　／　　╲☆\n☆　◢██◣   Sakura \n _▂▂█▂▂   Channel·2.0.0 \n<b>▎欢迎使用Funmika！${userName}</b>\n\n· 🆔 用户のID | ${userId}`
+                const caption = `☆　　╱╲*╱╲　☆\n ╱╳+▲╱　　╲　☆\n╱╱ ◢◣+　　╳╲ \n╱ +◢█◣　／　　╲☆\n☆　◢██◣   Sakura \n _▂▂█▂▂   Channel·2.0.0 \n▎欢迎使用Funmika！${userName}\n\n· 🆔 用户のID | ${userId}`;
+                const result = [
+                    [
+                        { text: "Github主页", url: "https://github.com/Sliverkiss" },
+                        { text: "Telegram频道", url: "https://t.me/sliverkiss_blog" }
+                    ],
+                    [{ text: "Give me a coffee☕️", url: "https://raw.githubusercontent.com/Sliverkiss/QuantumultX/main/icon/IMG_3486.jpeg" }]
+                ]
                 ctx.reply(caption, {
-                    parse_mode: 'HTML'
+                    parse_mode: 'HTML', reply_markup: {
+                        inline_keyboard: result
+                    }
                 });
             }
         } catch (e) {
