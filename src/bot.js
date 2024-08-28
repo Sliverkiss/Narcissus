37ed7813b5579f6601a52033d2e76591f7dac1d5
Sliverkiss <70052878+Sliverkiss@users.noreply.github.com>
2024-08-28 09:29:13 +0800

    reactor(config):转移config到System，修改相关引入路径

--- src/bot.js
+++ src/bot.js
@@ -1,11 +1,9 @@
 import { Telegraf } from 'telegraf';
-import PluginManager from './services/pluginManager.js';
+import PluginManager from './Module/services/pluginManager.js';
 import * as path from 'node:path';
-import { CONFIG } from './config/index.js';
-import { Model } from './database/index.js';
+import { CONFIG } from './System/Config/index.js';
 //全局属性
 global.config = CONFIG;
-global.Model = Model;
 
 const bot = new Telegraf(config.botToken);
 const pluginManager = new PluginManager(path.join(import.meta.dirname, 'plugins'));
--- src/config/index.js
+++
@@ -1,7 +0,0 @@
-(await import('dotenv')).config();
-
-export const CONFIG={
-    botToken: process.env.BOT_TOKEN || '7097873325:AAGvD5ctMZ_CiRzIGJZLh9RV7LMgCRSHEzo',
-    dbPath: process.env.DB_PATH || 'bot.sqlite',
-    adminId: process.env.ADMIN_ID || '5232284790'
-};
